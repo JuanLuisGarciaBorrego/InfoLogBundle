37969b428fb0aa5e8f055b02138190e180c607b8
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2015-10-26 19:22:01 +0100

    User-Agent is showing in log

--- EventListener/UserAgentSubscriber.php
+++ EventListener/UserAgentSubscriber.php
@@ -2,15 +2,32 @@
 
 namespace JuanLuisGarciaBorrego\InfoLogBundle\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class UserAgentSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * UserAgentSubscriber constructor
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
         $userAgent = $request->headers->get('User-Agent');
+
+        $this->logger->info($userAgent);
     }
 
     public static function getSubscribedEvents()
